e8718dbfc492548714747d0fe42716b135c33461
ayankovsky <p44gr10@gmail.com>
2020-01-25 23:53:47 +0100

    Implemented slack notifications for new projects to review

--- src/handler/submit-project/submit-project.ts
+++ src/handler/submit-project/submit-project.ts
@@ -1,6 +1,8 @@
 import { DataMapper } from '@aws/dynamodb-data-mapper'
 import * as DynamoDB from 'aws-sdk/clients/dynamodb'
+
 import { ProjectToReview } from '../../entity/submitted-project'
+import { slackService } from '../../service/slack.service'
 
 interface SubmitProjectReview {
   repositoryUrl: string
@@ -10,6 +12,10 @@ const mapper = new DataMapper({
   client: new DynamoDB(),
 })
 
-export const handler = async ({ repositoryUrl }: SubmitProjectReview) => (
-  mapper.put(Object.assign(new ProjectToReview(), { repositoryUrl }))
-)
+export const handler = async ({ repositoryUrl }: SubmitProjectReview) => {
+  const project = await mapper.put(Object.assign(new ProjectToReview(), { repositoryUrl }))
+
+  await slackService.sendMessage(`New project to review: ${repositoryUrl}`, process.env.reviewSlackChannel!)
+
+  return project
+}
